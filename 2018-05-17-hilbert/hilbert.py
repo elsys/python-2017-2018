5a0eefc13f70cf9a2e57cab91f631159d9b15153
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-05-17 14:10:05 +0300

    hilbert curve classwork...

---
+++ 2018-05-17-hilbert/hilbert.py
@@ -0,0 +1,94 @@
+import turtle
+
+
+class Hilbert(object):
+	
+	def __init__(self, step):
+		self.step = step
+		self.pen = turtle.Pen()
+	
+	def a(self, i):
+		if i<=0:
+			return
+	
+		self.d(i-1)
+		
+		self.pen.setheading(180)
+		self.pen.forward(self.step)
+		
+		self.a(i-1)
+		self.pen.setheading(270)
+		self.pen.forward(self.step)
+		
+		self.a(i-1)
+		self.pen.setheading(0)
+		self.pen.forward(self.step)
+		
+		self.b(i-1)
+
+	def b(self, i):
+		if i<= 0:
+			return
+		
+		self.c(i-1)
+		self.pen.setheading(90)
+		self.pen.forward(self.step)
+		
+		self.b(i-1)
+		self.pen.setheading(0)
+		self.pen.forward(self.step)
+		
+		self.b(i-1)
+		self.pen.setheading(270)
+		self.pen.forward(self.step)
+		
+		self.a(i-1)
+		
+	
+	def c(self, i):
+		if i<=0:
+			return
+		
+		self.b(i-1)
+		
+		self.pen.setheading(0)
+		self.pen.forward(self.step)
+		
+		self.c(i-1)
+		self.pen.setheading(90)
+		self.pen.forward(self.step)
+		
+		self.c(i-1)
+		self.pen.setheading(180)
+		self.pen.forward(self.step)
+		
+		self.d(i-1)
+	
+	def d(self, i):
+		if i<=0:
+			return
+		self.a(i-1)
+		
+		self.pen.setheading(270)
+		self.pen.forward(self.step)
+		
+		self.d(i-1)
+		
+		self.pen.setheading(180)
+		self.pen.forward(self.step)
+		
+		self.d(i-1)
+		
+		self.pen.setheading(90)
+		self.pen.forward(self.step)
+		
+		self.c(i-1)
+
+
+if __name__ == "__main__":
+	h = Hilbert(10)
+	h.d(7)
+	
+	
+	input("Press any key")
+
